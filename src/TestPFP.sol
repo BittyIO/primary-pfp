8c3fd790d0ded276049aac83fb498ee9d711e750
PirateCode <piratecodeweb3@gmail.com>
2025-03-13 10:12:43 +1100

    mint PFP one by one

--- src/TestPFP.sol
+++ src/TestPFP.sol
@@ -6,13 +6,14 @@ import {ERC721Enumerable} from "../lib/openzeppelin-contracts/contracts/token/ER
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 /**
- * @title BlueChips
- * @dev This contract is only for deployed on testnet for better testing.
+ * @title TestPFP
+ * @dev This contract is only for deployment on testnet for better testing.
  */
 contract TestPFP is Ownable, ERC721Enumerable {
     string public baseURI;
     uint256 public collectionLimit;
     mapping(address => uint256) public mintCounts;
+    uint256 private _currentTokenId = 0;
 
     constructor(string memory name, string memory symbol) Ownable() ERC721(name, symbol) {
         baseURI = "https://MintableERC721/";
@@ -20,17 +21,17 @@ contract TestPFP is Ownable, ERC721Enumerable {
     }
 
     /**
-     * @dev Function to mint tokens
-     * @param tokenId The id of tokens to mint.
+     * @dev Function to mint the next available token ID.
      * @return A boolean that indicates if the operation was successful.
      */
-    function mint(uint256 tokenId) external returns (bool) {
-        require(tokenId < collectionLimit, "exceed collection limit");
+    function mint() external returns (bool) {
+        require(_currentTokenId < collectionLimit, "exceed collection limit");
 
         mintCounts[_msgSender()] += 1;
         require(mintCounts[_msgSender()] <= 10, "exceed mint limit");
 
-        _mint(_msgSender(), tokenId);
+        _currentTokenId += 1;
+        _mint(_msgSender(), _currentTokenId);
         return true;
     }
 
